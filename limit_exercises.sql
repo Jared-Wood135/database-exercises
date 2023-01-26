ae83e305926e040fc15b290e8d02f00739eadaae
Jared Wood <jared.wood135@gmail.com>
2023-01-26 10:09:59 -0600

    Corrections made to questions 3 and 4 due to incorrect conditional set to hire_date rather than birth_date

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,15 +4,15 @@ SHOW TABLES;
 SELECT * FROM employees LIMIT 5;
 SELECT DISTINCT title FROM titles;
 SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
-SELECT first_name, last_name, hire_date FROM employees WHERE hire_date >= '1990-01-01' AND hire_date <= '1999-12-31' AND hire_date LIKE '%12-31%' ORDER BY hire_date LIMIT 5;
-SELECT first_name, last_name, hire_date FROM employees WHERE hire_date >= '1990-01-01' AND hire_date <= '1999-12-31' AND hire_date LIKE '%12-31%' ORDER BY hire_date LIMIT 5 OFFSET 45;
+SELECT first_name, last_name, hire_date FROM employees WHERE hire_date >= '1990-01-01' AND hire_date <= '1999-12-31' AND birth_date LIKE '%12-31%' ORDER BY hire_date LIMIT 5;
+SELECT first_name, last_name, hire_date FROM employees WHERE hire_date >= '1990-01-01' AND hire_date <= '1999-12-31' AND birth_date LIKE '%12-31%' ORDER BY hire_date LIMIT 5 OFFSET 45;
 /*
 
 START EXERCISE
 
 2. "Zykh", "Zyda", "Zwicker", "Zweizig", "Zumaque", "Zultner", "Zucker", "Zuberek", "Zschoche", "Zongker"
-3. "Dipayan Hennebert", "Mohammad Zallocco", "Supot Aingworth", "Kwangyoen Sabnani", "Froduald Koshino"
-4. "Serge Thombley", "Nobuyoshi Vrecion", "Sahrah Roccetti", "Persi SChlumberger", "Yolla Ratnaker"
+3. "Magalena Guting", "Chaosheng Plavsic", "Sachin Brodie", "Douadi Streit", "Ymte Schlegelmilch"
+4. "Tomofumi Ducloy", "Willard Itschner", "Ziyad Schueller", "Claude Ventosa", "Alenka Garrabrants"
 
 END EXERCISE
 
