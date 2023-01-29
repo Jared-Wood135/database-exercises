7c7c8e98873810d327b0ee2a083d03dda0f41f3f
Jared Wood <jared.wood135@gmail.com>
2023-01-29 11:52:53 -0600

    Completed more of the exercises

--- more_exercises.sql
+++ more_exercises.sql
@@ -35,3 +35,34 @@ SELECT * FROM address LIMIT 100;
 SELECT phone, district FROM address WHERE district IN ('California', 'England', 'Taipei', 'West Java');
 SELECT * FROM payment LIMIT 5;
 SELECT payment_id, amount, payment_date FROM payment WHERE DATE(payment_date) IN ('2005-05-25', '2005-05-27', '2005-05-29');
+SHOW DATABASES;
+USE sakila;
+SHOW TABLES;
+SELECT * FROM film LIMIT 10;
+SELECT * FROM film WHERE rating IN ('G', 'PG-13', 'NC-17');
+
+-- BETWEEN operator
+SELECT * FROM payment LIMIT 10;
+SELECT * FROM payment WHERE payment_date BETWEEN '2005-05-26 00:00:00' AND '2005-05-26  23:59:59';
+SELECT film_id, title, description from film WHERE LENGTH(description) BETWEEN 100 AND 120;
+
+-- LIKE operator
+SELECT * FROM film WHERE description LIKE 'A Thoughtful%';
+SELECT * FROM film WHERE description LIKE '%Boat';
+SELECT * FROM film WHERE description LIKE '%Database%' AND length > 180;
+
+-- LIMIT operator
+SELECT * FROM payment LIMIT 20;
+SELECT payment_date, amount FROM payment WHERE amount > 5 AND payment_id BETWEEN 1000 AND 2000;
+SELECT * FROM customer LIMIT 10;
+SELECT * FROM customer WHERE customer_id BETWEEN 101 AND 200;
+
+-- ORDER BY statement
+SELECT * FROM film ORDER BY length;
+SELECT DISTINCT rating FROM film ORDER BY rating DESC;
+SELECT * FROM payment LIMIT 10;
+SELECT payment_date, amount FROM payment ORDER BY amount DESC LIMIT 20;
+SELECT title, description, special_features, length, rental_duration from film WHERE length < 120 AND rental_duration BETWEEN 5 AND 7 ORDER BY length DESC LIMIT 10;
+
+
+-- JOIN statement
