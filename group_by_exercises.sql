824801a138c9580d8866195e58dfe2808121e73d
Jared Wood <jared.wood135@gmail.com>
2023-01-30 10:48:00 -0600

    Completed bonus exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -80,17 +80,52 @@ SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, AVG(salary) AS avg_sa
 
 /*  Using the dept_emp table, count how many current employees work in each department.
 The query result should show 9 rows, one for each department and the employee count.  */
+SHOW TABLES;
+SELECT * FROM departments LIMIT 10;
+SELECT * FROM dept_emp LIMIT 10; -- Link dept_no with dept_name and COUNT(emp_no) AS 1 integer
+SELECT dept_name, COUNT(emp_no) AS total_employees
+	FROM departments NATURAL JOIN dept_emp
+    GROUP BY dept_name;
 
 -- Determine how many different salaries each employee has had. This includes both historic and current.
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, COUNT(salary) AS total_different_salaries
+	FROM employees NATURAL JOIN salaries
+    GROUP BY emp_no, name
+    LIMIT 10;
 
 --  Find the maximum salary for each employee.
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, MAX(salary) AS highest_salary
+	FROM employees NATURAL JOIN salaries
+    GROUP BY emp_no, name
+    LIMIT 10;
 
 -- Find the minimum salary for each employee.
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, MIN(salary) AS lowest_salary
+	FROM employees NATURAL JOIN salaries
+    GROUP BY emp_no, name
+    LIMIT 10;
 
 -- Find the standard deviation of salaries for each employee.
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, STDDEV(salary) AS stddev_salary
+	FROM employees NATURAL JOIN salaries
+    GROUP BY emp_no, name
+    ORDER BY stddev_salary
+    LIMIT 10;
 
 -- Now find the max salary for each employee where that max salary is greater than $150,000.
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, MAX(salary) AS highest_salary
+	FROM employees NATURAL JOIN salaries
+    GROUP BY emp_no, name
+    HAVING highest_salary > 150000
+    ORDER BY highest_salary DESC
+    LIMIT 10;
 
 -- Find the average salary for each employee where that average salary is between $80k and $90k.
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, AVG(salary) AS average_salary
+	FROM employees NATURAL JOIN salaries
+    GROUP BY emp_no, name
+    HAVING average_salary BETWEEN 80000 AND 90000
+    ORDER BY average_salary
+    LIMIT 10;
 
 -- END BONUS EXERCISES: --
