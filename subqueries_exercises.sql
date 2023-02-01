7a148ad43b0a883ef2101036fa8de1cf5b5ded4c
Jared Wood <jared.wood135@gmail.com>
2023-01-31 20:57:19 -0600

    Created for subqueries exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,35 @@
+-- EXERCISE START --
+
+-- 1. Find all the current employees with the same hire date as employee 101010 using a subquery.
+
+-- 2. Find all the titles ever held by all current employees with the first name Aamod.
+
+/* 3. How many people in the employees table are no longer working for the company? 
+Give the answer in a comment in your code.*/
+
+-- 4. Find all the current department managers that are female. List their names in a comment in your code.
+
+/*5. Find all the employees who currently have a higher salary than the companies overall, 
+historical average salary.*/
+
+/*6. How many current salaries are within 1 standard deviation of the current highest salary?
+What percentage of all salaries is this?*/
+
+-- EXERCISE END --
+
+/*
+
+BREAK LINE FROM EXERCISE TO BONUS EXERCISE
+
+*/
+
+-- BONUS EXERCISE START --
+
+-- 1. Find all the department names that currently have female managers.
+
+-- 2. Find the first and last name of the employee with the highest salary.
+
+-- 3. Find the department name that the employee with the highest salary works in.
+
+
+-- BONUS EXERCISE END --
