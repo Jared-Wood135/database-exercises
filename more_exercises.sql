c50d4c2657661b259fa1bbc1928bc97c97447ace
Jared Wood <jared.wood135@gmail.com>
2023-02-02 11:14:17 -0600

    Completed 1st set of 9 questions under the sakila exercises from the curriculum

--- more_exercises.sql
+++ more_exercises.sql
@@ -178,20 +178,42 @@ SELECT CONCAT(customer.first_name, ' ', customer.last_name) AS customer_first_na
 an inner join between the customer and actor column on the last_name column in each table. (i.e. 
 customer.last_name = actor.last_name)
 returns correct number of records: 43*/
+SELECT customer.first_name, customer.last_name, actor.first_name, actor.last_name
+	FROM customer
+		INNER JOIN actor USING(last_name);
 
 /* 9d. Select the city name and country name columns from the city table, performing a left join with 
 the country table to get the country name column.
 Returns correct records: 600*/
+SHOW TABLES;
+SELECT * FROM country LIMIT 10;
+SELECT * FROM city LIMIT 10;
+SELECT city.city, country.country
+	FROM city
+		LEFT JOIN country USING(country_id);
 
 /* 9e. Select the title, description, release year, and language name columns from the film table, 
 performing a left join with the language table to get the "language" column.
 Label the language.name column as "language"
 Returns 1000 rows*/
+DESCRIBE film;
+DESCRIBE language;
+SELECT film.title, film.description, film.release_year, film.original_language_id, language.name AS language
+	FROM film
+		LEFT JOIN language USING(language_id);
 
 /* 9f. Select the first_name, last_name, address, address2, city name, district, and postal code columns 
 from the staff table, performing 2 left joins with the address table then the city table to get the 
 address and city related columns.
 returns correct number of rows: 2*/
+SHOW TABLES;
+DESCRIBE staff;
+DESCRIBE address;
+DESCRIBE city;
+SELECT staff.first_name, staff.last_name, address.address, address.address2, city.city, address.district, address.postal_code
+	FROM staff
+		LEFT JOIN address USING(address_id)
+        LEFT JOIN city USING(city_id);
 
 -- 1ST SET OF QUESTIONS (9) END --
 
