1093026c138993d8a3242a20f472b4252cc8a504
Jared Wood <jared.wood135@gmail.com>
2023-02-02 16:14:25 -0600

    Additional exercises completed, but entire worksheet incomplete

--- more_exercises.sql
+++ more_exercises.sql
@@ -222,3 +222,121 @@ SELECT staff.first_name, staff.last_name, address.address, address.address2, cit
 BREAK LINE FROM 1ST SET OF QUESTIONS (9) TO 2ND SET OF QUESTIONS (19)
 
 */
+
+-- 2ND SET OF QUESTIONS (19) START --
+
+-- 1. Display the first and last names in all lowercase of all the actors.
+USE sakila;
+DESCRIBE actor;
+SELECT LOWER(first_name), LOWER(last_name)
+	FROM actor;
+
+/* 2. You need to find the ID number, first name, and last name of an actor, of whom you know only 
+the first name, "Joe." What is one query would you could use to obtain this information?*/
+DESCRIBE actor;
+SELECT actor_id, first_name, last_name
+	FROM actor
+    WHERE first_name = 'Joe';
+
+-- 3. Find all actors whose last name contain the letters "gen":
+SELECT *
+	FROM actor
+    WHERE last_name LIKE '%gen%';
+
+/* 4. Find all actors whose last names contain the letters "li". This time, order the rows by 
+last name and first name, in that order.*/
+SELECT *
+	FROM actor
+    WHERE last_name LIKE '%li%'
+    ORDER BY last_name, first_name;
+
+/* 5. Using IN, display the country_id and country columns for the following countries: 
+Afghanistan, Bangladesh, and China:*/
+DESCRIBE country;
+SELECT country_id, country
+	FROM country
+    WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
+
+-- 6. List the last names of all the actors, as well as how many actors have that last name.
+SELECT last_name, COUNT(*)
+	FROM actor
+    GROUP BY last_name;
+
+/* 7. List last names of actors and the number of actors who have that last name, but only 
+for names that are shared by at least two actors*/
+SELECT last_name, COUNT(*) AS occurances
+	FROM actor
+    GROUP BY last_name
+    HAVING occurances >= 2;
+
+-- 8. You cannot locate the schema of the address table. Which query would you use to re-create it?
+SHOW DATABASES;
+SHOW TABLES;
+SELECT table_schema
+	FROM my_codeup_connection.tables
+    WHERE table_name LIKE '%address%';
+
+-- 9. Use JOIN to display the first and last names, as well as the address, of each staff member.
+SHOW TABLES;
+SELECT * FROM staff;
+DESCRIBE staff;
+DESCRIBE address;
+SELECT staff.first_name, staff.last_name, address.address
+	FROM staff
+		JOIN address USING(address_id);
+
+-- 10. Use JOIN to display the total amount rung up by each staff member in August of 2005.
+SHOW TABLES;
+SELECT * FROM staff; -- staff_id, first_name, last_name
+SELECT * FROM payment; -- staff_id, amount
+SELECT CONCAT(staff.first_name, ' ', staff.last_name) as name, SUM(payment.amount) AS total
+	FROM staff
+		JOIN payment USING(staff_id)
+	WHERE payment.payment_date LIKE '2005-08%'
+    GROUP BY name;
+
+-- 11. List each film and the number of actors who are listed for that film.
+SHOW TABLES;
+SELECT * FROM actor LIMIT 10; -- actor_id
+SELECT * FROM film_actor LIMIT 10; -- actor_id, film_id
+SELECT * FROM film LIMIT 10; -- title, film_id
+SELECT film.title AS film_title, COUNT(*) AS total_actors
+	FROM film
+		JOIN film_actor USING(film_id)
+        JOIN actor USING(actor_id)
+	GROUP BY film.title;
+    
+-- 12. How many copies of the film Hunchback Impossible exist in the inventory system?
+SELECT * FROM inventory LIMIT 10; -- film_id
+SELECT * FROM film LIMIT 10; -- film_id
+SELECT film.title, COUNT(*) AS total_copies
+	FROM film
+		JOIN inventory USING(film_id)
+	WHERE film.title = 'Hunchback Impossible';
+
+/* 13. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended 
+consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries 
+to display the titles of movies starting with the letters K and Q whose language is English.*/
+
+-- 14. Use subqueries to display all actors who appear in the film Alone Trip.
+
+/* 15. You want to run an email marketing campaign in Canada, for which you will need the names and 
+email addresses of all Canadian customers.*/
+
+/* 16. Sales have been lagging among young families, and you wish to target all family movies for a 
+promotion. Identify all movies categorized as famiy films.*/
+
+-- 17. Write a query to display how much business, in dollars, each store brought in.
+
+-- 18. Write a query to display for each store its store ID, city, and country.
+
+/* 19. List the top five genres in gross revenue in descending order. (Hint: you may need to use 
+the following tables: category, film_category, inventory, payment, and rental.)*/
+
+-- 2ND SET OF QUESTIONS (19) END --
+
+/*
+
+BREAK LINE FROM 2ND SET OF QUESTIONS (19) TO 3RD SET OF QUESTIONS (7)
+
+*/
